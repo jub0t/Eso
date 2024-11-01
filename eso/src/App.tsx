0ad6dc34b5526093bf3910d32e8daa634942c966
Jareer <jub0t@proton.me>
2024-11-01 09:02:01 +0500

    updates

--- eso/src/App.tsx
+++ eso/src/App.tsx
@@ -2,8 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="">
+    <main className="h-screen flex flex-wrap w-full overflow-x-hidden">
+      <section className="w-[10%] border-right-2 border-gray-600">
 
+      </section>
+
+      <section className="w-[90%]">
+
+      </section>
     </main>
   );
 }
---
+++ eso/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./src/**/*.{vue,js,ts}'],
+    theme: {
+        // Your custom theme configuration
+    },
+    variants: {},
+    plugins: [],
+}
