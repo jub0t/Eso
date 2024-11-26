43aa7611fdda54707bacbfec0b6a2f1cd1ad7f17
Jareer <jub0t@proton.me>
2024-11-26 20:00:50 +0500

    updates

--- eso/src-tauri/src/database/mod.rs
+++ eso/src-tauri/src/database/mod.rs
@@ -1,6 +1,18 @@
 use serde::{Deserialize, Serialize};
+use std::borrow:Cow;
+use surrealdb::{Surreal};
 
 #[derive(Serialize, Deserialize)]
 struct Key {
     name: Cow<'static, str>,
+}
+
+struct Core {}
+impl Core {
+    pub fn new()->Self{
+        let db = Surreal::new();
+        return Self {
+
+        };
+    }
 }
